2e70e39fb88bc134b3354df8be6e6b2235605a08
samuel <samuelarefeaynie2@gmail.com>
2022-08-25 11:52:49 +0300

    started survey page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
-
+import Surveys from "./pages/Surveys";
 
 function App() {
   return (
     <div className="App">
+      <Header />
 
-      <Header/>
-
+      <Surveys />
     </div>
   );
 }
---
+++ src/pages/Surveys.js
@@ -0,0 +1,19 @@
+const Surveys = () => {
+  return (
+    <div className="container border-2 border-black mx-auto mt-10 p-12">
+      <div className="text-5xl text-brandBrown font-bold">Surveys</div>
+      <div className="border-2 w-full my-3 h-96 flex flex-row flex-wrap justify-start items-start overflow-y-scroll">
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+        <div className="border-2 w-80 border-black h-64 my-2 mr-10"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Surveys;
